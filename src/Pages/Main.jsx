cefbfca91bc4adbf81eff7b8628725b7511986e0
TusharV27 <tusharvaghasiya369@gmail.com>
2024-06-24 14:26:10 +0530

    add meta

--- src/Pages/Main.jsx
+++ src/Pages/Main.jsx
@@ -9,6 +9,7 @@ import Service from "./Service/Service";
 import Facility from "./Facility/Facility";
 import Gallery from "./Gallery/Gallery";
 import ContactUs from "./ContactUs/ContactUs";
+import { Helmet } from "react-helmet";
 
 function Main() {
   const [language, setLanguage] = useState(
@@ -25,6 +26,40 @@ function Main() {
   };
   return (
     <div>
+      <Helmet>
+        <title>Gajera Children Hospital</title>
+        <meta
+          name="description"
+          content="Gajera Children Hospital provides top-notch pediatric care with a dedicated team of experienced doctors."
+        />
+        <link
+          rel="canonical"
+          href="https://gajera-children-hospital1.vercel.app"
+        />
+        <meta
+          name="keywords"
+          content="Gajera Children Hospital, pediatric care, children hospital, neonatology, pediatric services, child healthcare, emergency pediatric care"
+        />
+        <meta name="author" content="Gajera Children Hospital" />
+        <meta property="og:title" content="Gajera Children Hospital" />
+        <meta
+          property="og:description"
+          content="Providing exceptional pediatric care with a focus on your child's health and well-being."
+        />
+        <meta
+          property="og:url"
+          content="https://gajera-children-hospital1.vercel.app"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="URL_to_your_og_image.jpg" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Gajera Children Hospital" />
+        <meta
+          name="twitter:description"
+          content="Exceptional pediatric care with a dedicated team of experienced doctors."
+        />
+        <meta name="twitter:image" content="URL_to_your_twitter_image.jpg" />
+      </Helmet>
       <BrowserRouter>
         <div>
           <Navbar
